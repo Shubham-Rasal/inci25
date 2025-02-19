59eb60b8b83d868b0d1e8bdb79d876ca2501133d
mohnishraj317 <mohnishrajmani317@gmail.com>
2025-02-19 20:14:32 +0530

    added : responsive footer

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -1,80 +1,84 @@
 import * as React from 'react';
+import Link from 'next/link';
+import { Facebook } from 'lucide-react';
 
-
-export const NavItem = ({ text, isHighlighted }) => (
-  <div className={`mt-1 ${isHighlighted ? 'text-orange-400' : ''}`}>
-    {text}
-  </div>
-);
-
-
-export const SocialIcon = ({ src, alt }) => (
-  <img
+export const SocialIcon = ({ src, elem, alt }) => (
+  elem ? elem : <img
     loading="lazy"
     src={src}
     alt={alt}
-    className="object-contain shrink-0 aspect-square w-[51px]"
+    className="object-contain shrink-0 aspect-square size-[32px]"
   />
 );
 
 const navigationData = [
-  ['Home', 'Team', 'Sponsors'],
-  ['Merchandise', 'Pro-show', 'Events'],
-  ['I-care', 'FAQs', 'About us']
+  [
+    { display: 'Home', link: "#", id: 1 },
+    { display: 'Team', link: "#", id: 1 },
+    { display: 'Sponsors', link: "#", id: 1 },
+  ], [
+    { display: 'Merchandise', link: "#", id: 1 },
+    { display: 'Pro-show', link: "#", id: 1, active: true },
+    { display: 'Events', link: "#", id: 1 },
+  ], [
+    { display: 'I-care', link: "#", id: 1 },
+    { display: 'FAQs', link: "#", id: 1 },
+    { display: 'About us', link: "#", id: 1 }
+  ]
 ];
 
-export const Navigation = () => (
-  <div className="flex flex-wrap gap-10 text-xl tracking-wide leading-none text-yellow-800 max-md:mr-2 max-md:max-w-full">
-    {navigationData.map((column, columnIndex) => (
-      <div 
-        key={columnIndex} 
-        className={`flex flex-col ${
-          columnIndex === 1 
-            ? 'whitespace-nowrap w-[127px]' 
-            : 'flex-1 shrink whitespace-nowrap basis-0'
-        }`}
-      >
-        {column.map((item, itemIndex) => (
-          <NavItem 
-            key={itemIndex} 
-            text={item} 
-            isHighlighted={item === 'Pro-show'}
-          />
-        ))}
-      </div>
-    ))}
-  </div>
-);
+function Navlink({ display, link, active }) {
+  return <Link className={`${active ? "text-[#C99F36]" : "text-[#755D21]"}`}
+    style={{ textShadow: active ? "0 0 .4rem #DBAE3D" : "" }}
+    href={link}>{display}</Link>
+}
 
+function Navlist({ items }) {
+  return <ul className="flex flex-col">
+    {items.map(data => <Navlink {...data} key={data.id} />)}
+  </ul>
+}
+
+function Navigation() {
+  return <div className="flex gap-4 flex-wrap items-center justify-between">
+    {navigationData.map(data => <Navlist items={data} />)}
+  </div>
+}
 
 const socialIcons = [
-  { src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/d4aa904acdcbc811188b1c1bda2b9fcf0ff464920a8135fe749b20b2630ced3a?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 1' },
-  { src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/2debe8bb03a9dea3f94dde4248857130164b36a0c82b1caaa637eed082655ab3?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 2' },
-  { src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e797c8e0284b2f1062f634fd44f2c6a194a6b763ad69ee39c588ae0e56d70a94?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 3' }
+  { id: 1, src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/d4aa904acdcbc811188b1c1bda2b9fcf0ff464920a8135fe749b20b2630ced3a?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 1' },
+  { id: 2, elem: <Facebook className="text-[#755D21] size-[32px]" /> },
+  // { id: 2, src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/2debe8bb03a9dea3f94dde4248857130164b36a0c82b1caaa637eed082655ab3?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 2' },
+  { id: 3, src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e797c8e0284b2f1062f634fd44f2c6a194a6b763ad69ee39c588ae0e56d70a94?placeholderIfAbsent=true&apiKey=0def968ddfc54ad08cb5c8196cfa1cf5', alt: 'Social Media Icon 3' }
 ];
 
+function SocialIcons({ socialIcons }) {
+  return <div className="flex gap-4">
+    {socialIcons.map(icon => <SocialIcon {...icon} key={icon.id} />)}
+  </div>
+}
+
+function ContactEmail() {
+  return <div className="text-[#D1BD86]">
+    <span>Contact Us :</span>
+    <br />
+    <span>incidentnitk@gmail.com</span>
+  </div>
+}
+
 export const Footer = () => {
-  return (
-    <div className="flex overflow-hidden flex-wrap gap-10 items-start pt-20 pr-20 pl-7 max-md:px-5 bg-gradient-to-t from-background to-background-gradient">
-      <div className="grow self-end mt-44 text-[180px] max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-        incident
-      </div>
-      <div className="shrink-0 self-start w-0 h-[372px]" />
-      <div className="flex flex-col my-auto max-md:max-w-full">
-        <Navigation />
-        <div className="flex flex-wrap gap-10 items-center mt-36 max-md:mt-10 max-md:max-w-full">
-          <div className="self-stretch my-auto text-xl tracking-wide leading-6 text-orange-300">
-            <span className="text-orange-300">contact us:</span>
-            <br />
-            <span className="text-2xl">incidentnitk@gmail.com</span>
-          </div>
-          <div className="flex gap-10 justify-center items-end self-stretch my-auto">
-            {socialIcons.map((icon, index) => (
-              <SocialIcon key={index} {...icon} />
-            ))}
-          </div>
-        </div>
+  return <div className="relative grid grid-cols-2 bg-gradient-to-b from-[#1E0E03] via-[#120802] to-black items-end font-mono p-8 max-md:grid-cols-1">
+    <div className="text-[7vw] max-md:text-[8vw]">Incident</div>
+
+    <span className="max-md:hidden w-[1px] -translate-x-full absolute top-0 left-1/2 block h-full bg-gradient-to-b from-transparent via-[#D1BD86] to-transparent " />
+
+    <div className="flex flex-col gap-[20vh] pl-4 max-md:pl-0 justify-between">
+      <Navigation />
+
+      <div className="flex justify-between items-end gap-4 flex-wrap">
+        <ContactEmail />
+        <SocialIcons socialIcons={socialIcons} />
       </div>
     </div>
-  );
+  </div >
 }
